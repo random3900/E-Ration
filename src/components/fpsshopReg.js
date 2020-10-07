799d5b3c49367cb207d61e6df78f16161b9bbc9b
Roopa Vidhya G <61742380+random3900@users.noreply.github.com>
2020-10-08 00:43:15 +0530

    Add Sample Phone Number Validation

--- src/components/fpsshopReg.js
+++ src/components/fpsshopReg.js
@@ -10,22 +10,49 @@ class fpsshopReg extends React.Component {
     super();
     this.state = {};
   }
+  validateInput(inputs) {
+    //Define Later
+    console.log(inputs);
+
+    //Phone Number Validation
+    var ph1 = inputs["Phone number 1"];
+    var ph2 = inputs["Phone number 2"];
+    if (ph1.length < 10 || ph2.length < 10) return "Phone Number is Incorrect";
+
+    return "0";
+  }
+  insertIntoSQL(inputs, tablename) {}
+  insertValues() {
+    console.log(this.validateInput(this.state));
+    //this.insertIntoSQL(this.state, this.newMethod());
+  }
+  newMethod() {
+    return "Customer";
+  }
+
   render() {
     return (
       <div>
         <Header />
         <div class="container">
           <Heading title="FPS Shop Registration" />
-          <Form fields={Fields.nameFields}></Form>
-          <Form fields={Fields.addrFields1} />
-          <Form fields={Fields.addrFields2} />
-          <Form fields={Fields.timeField} />
-          <Form fields={Fields.noofcustomersField} />
-          <Form fields={Fields.phonenumberField} />
-          <Form fields={Fields.bankdetailsField} />
-          <Form fields={Fields.idField} />
+          {Object.keys(Fields).map((key) => {
+            return (
+              <Form
+                fields={Fields[key]}
+                onChangeHandler={(event) => {
+                  this.setState({ [event.target.name]: event.target.value });
+                  console.log(this.state);
+                }}
+              />
+            );
+          })}
 
-          <button type="submit" class="btn btn-primary">
+          <button
+            type="submit"
+            class="btn btn-primary"
+            onClick={this.insertValues.bind(this)}
+          >
             Submit
           </button>
         </div>
